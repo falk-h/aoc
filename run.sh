7463e7b7a5bfdd8e0bbe5e320a199194d992c6a2
Falk Höppner <falk@hoppner.se>
2020-12-02 19:12:51 +0100

    Add a script to run the solutions

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+cd "$(dirname "$0")" || exit 1
+if ! [ $# -ge 2 ]; then
+    echo "Usage: $0 DAY PART [FLAGS]"
+    exit 1
+fi
+day="$1"
+part="$2"
+shift 2
+exec cargo run "$@" --bin "day$day-part$part"
